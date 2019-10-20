ae5295aa74b00b0d0d4bc3cb1933f03858917e10
Marie Altintopi <mariealtin@live.co.za>
2019-10-20 13:20:32 +0200

    basic post added and some refactoring

--- PhoneBook/Controllers/HomeController.cs
+++ PhoneBook/Controllers/HomeController.cs
@@ -10,21 +10,25 @@ namespace PhoneBook.Controllers
 {
     public class HomeController : Controller
     {
-        // GET all contacts without search criteria
+        private const string BASE_ADDRESS = "http://localhost:51846/api/phonebook";
+
+        // GET all contacts
         public ActionResult Index(string searchTerm)
         {
-            string queryPath = "phonebook";
+            // TODO: improve this parameter string, possibly use uribuilder
+            string urlString = "";
             if (!String.IsNullOrEmpty(searchTerm))
             {
-                queryPath += String.Format(("?searchTerm={0}"), searchTerm);
+                urlString = String.Format(("?searchTerm={0}"), searchTerm);
             }
+
             var contacts = Enumerable.Empty<ContactViewModel>();
 
             using (var client = new HttpClient())
             {
-                client.BaseAddress = new Uri("http://localhost:51846/api/");
+                client.BaseAddress = new Uri(BASE_ADDRESS);
                 
-                var response = client.GetAsync(queryPath);
+                var response = client.GetAsync(urlString);
                 response.Wait();
 
                 var result = response.Result;
@@ -42,6 +46,34 @@ namespace PhoneBook.Controllers
 
             }
             return View(contacts);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // Create a contact
+        [HttpPost]
+        public ActionResult Create(ContactViewModel contact)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BASE_ADDRESS);
+
+                var post = client.PostAsJsonAsync<ContactViewModel>("phonebook", contact);
+                post.Wait();
+
+                var result = post.Result;
+                if(result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, "An error has Occurred");
+
+            return View(contact);
         }
     }
 }
